60399b6bf39bed630b648608a87cd1cdc189a2c1
Kobchok Chuawong <kobchok.chuawong@gmail.com>
2025-08-10 16:05:54 +0700

    new code

--- public/js/edit-waste.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const currentDateElement = document.querySelector(".current-date");
-    const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
-    const today = new Date();
-    const formattedDate = today.toLocaleDateString('en-US', options);
-
-    // Set the formatted date
-    currentDateElement.textContent = `Date : ${formattedDate}`;
-});
